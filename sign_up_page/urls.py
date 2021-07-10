4457e21237112a52d0aa4aec1b41432df8cbd7b1
Omkar Jahagirdar <omkar3602@gmail.com>
2021-07-11 03:06:43 +0530

    spelling correction Omkar J

--- sign_up_page/urls.py
+++ sign_up_page/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('login/',views.login,name='login_page'),
     path('logout/',views.logout,name='logout'),
 
-    #might be use full in settings sections
+    #might be useful in settings sections
     #https://www.youtube.com/watch?v=qjlZWBbX7-o
     #
     # path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),name='password_change_complete'),
