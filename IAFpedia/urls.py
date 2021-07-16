fd63b04076615fe643389e1d69ff716d79bb220c
Omkar Jahagirdar <omkar3602@gmail.com>
2021-07-16 13:40:54 +0530

    Historical Events Done Omkar J and Abhishek

--- IAFpedia/urls.py
+++ IAFpedia/urls.py
@@ -17,8 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('main_page.urls')),
     path('',include('sign_up_page.urls')),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
