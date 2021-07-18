9389dbf705d5d494242431def7652633113747de
Omkar Jahagirdar <omkar3602@gmail.com>
2021-07-18 13:03:32 +0530

    army main page(f end and b end) & navy&af(fend) OJ

--- main_page/admin.py
+++ main_page/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import BlogPost, HistoricalEvent
+from .models import BlogPost, HistoricalEvent, ArmyExam, NavyExam, AirforceExam
 # Register your models here.
 admin.site.register(BlogPost)
 admin.site.register(HistoricalEvent)
+
+class ExamAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+admin.site.register(ArmyExam, ExamAdmin)
+admin.site.register(NavyExam, ExamAdmin)
+admin.site.register(AirforceExam, ExamAdmin)
