dfd8c4ee97316410fcb21a47f0f5f49f0a831265
Omkar Jahagirdar <omkar3602@gmail.com>
2021-08-07 17:22:44 +0530

    Added project info

--- start.py
+++ start.py
@@ -1,3 +1,13 @@
+
+
+# IAFpedia-Website for Defence Examinations
+# 2 Django Apps 1)Main content 2)User authentication
+# 30 HTML Files
+# 30 CSS Files
+# 5 JavaScript Files
+
+
+
 try:
     import os
     os.system('python manage.py runserver')
